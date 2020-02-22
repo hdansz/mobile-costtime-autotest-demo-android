8f87f528db9bafb7e0680fb75f6ebbecc0e6f0b9
lhdandan <lhdandan@yeah.net>
2020-02-22 20:28:58 +0800

    'deleted'

--- djfortest.py
+++
@@ -1,9 +0,0 @@
-#encoding utf-8
-from Tools import compimgs_similar
-import os
-
-if __name__ == "__main__":
-    path = os.path.dirname(os.path.abspath(__file__))
-    p1 = path + "/temp_screenshot/" + "test.jpg"
-    p2 = path + "/expected_resultimg/" + "except.jpg"
-    compimgs_similar.runImgSimilar(p1, p2)
